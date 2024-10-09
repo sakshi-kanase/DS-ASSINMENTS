e8f0e7a67ae0d3632c4a0bcc920a5585905c68af
sakshi-kanase <kanasesakshi55@gmail.com>
2024-10-09 10:02:56 +0530

    Add files via upload

---
+++ 2DARRAY.C
@@ -0,0 +1,36 @@
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a[2][2],b[2][2],c[2][2],i,j,k;
+clrscr();
+printf("enter the value a\n");
+for(i=0; i<2; i++)
+{
+ for(j=0;j<2;j++)
+ {
+ scanf("%d",&a[i][j]);
+ }
+}
+printf("enter the value b\n");
+for(i=0;i<2;i++)
+{
+  for(j=0;j<2;j++)
+  {
+  scanf("%d",&b[i][j]);
+  }
+}
+printf("add of two value: \n");
+for(i=0;i<2;i++)
+{
+ for(j=0;j<2;j++)
+  {
+  c[i][j]=a[i][j]+b[i][j];
+  printf("%d\n",c[i][j]);
+  }
+}
+getch();
+return 0;
+}
+
