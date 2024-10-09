a741d4a5df6242a2de30bff5c2054e87e17f654d
sakshi-kanase <kanasesakshi55@gmail.com>
2024-10-09 20:50:12 +0530

    Update Assi4_SetB_Q1.c

--- Assi4_SetB_Q1.c
+++ Assi4_SetB_Q1.c
@@ -1,16 +1,13 @@
 //Write a C program to implement a Doubly linked list with Create and Display operation
 
 #include <stdio.h>
-#include <stdlib.h>
-
-// Structure for a doubly linked list node
+#include <conio.h>
 struct Node {
     int data;
     struct Node* next;
     struct Node* prev;
 };
 
-// Function to create a new node and insert it at the end of the doubly linked list
 void create(struct Node** head_ref, int new_data) {
     struct Node* new_node = (struct Node*) malloc(sizeof(struct Node));
     struct Node* last = *head_ref;
@@ -18,24 +15,20 @@ void create(struct Node** head_ref, int new_data) {
     new_node->data = new_data;
     new_node->next = NULL;
 
-    // If the list is empty, make the new node the head
     if (*head_ref == NULL) {
         new_node->prev = NULL;
         *head_ref = new_node;
         return;
     }
 
-    // Otherwise, traverse to the last node
     while (last->next != NULL) {
         last = last->next;
     }
 
-    // Update the last node's next and new node's previous pointers
     last->next = new_node;
     new_node->prev = last;
 }
 
-// Function to display the doubly linked list
 void display(struct Node* node) {
     struct Node* last;
     printf("Forward traversal: \n");
