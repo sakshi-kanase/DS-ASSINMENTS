1c15c597829967d1fbb0bb52acb03eb6ae53682d
sakshi-kanase <kanasesakshi55@gmail.com>
2024-10-09 10:03:40 +0530

    Add files via upload

---
+++ ARRAY.C
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a [2] [2],i,j;
+printf("Enter Array Elements");
+for (i=0; i<2; i++)
+{
+for(j=0;i<2;j++)
+{
+scanf("%d"a[i] [j]);
+}
+}
+printf("Matrix Elements\n");
+for(i=0; i<2; i++)
+{
+for(j=0; j<2; j++)
+{
+printf("%d",a [i] [i]);
+}
+printf("\n");
+}
+return0;
+}
